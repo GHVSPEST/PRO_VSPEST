b011b12f6892f9e1d5d84301bb129954b24849b7
NoNiQu <marcos.workstation@gmail.com>
2025-12-06 16:22:51 +0100

    cambio de icono

--- app/root.tsx
+++ app/root.tsx
@@ -51,7 +51,7 @@ export const links = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "icon", type: "image/svg+xml", href: "/iconos/LOGO.svg" },
+  { rel: "icon", type: "image/svg+xml", href: "/iconos/LOGO.png" },
 
   // ✅ Preload responsivo del hero
   {
